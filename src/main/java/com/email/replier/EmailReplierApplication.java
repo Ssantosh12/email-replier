9749e239c9bc4378990a58019d5b95a0fba2a7db
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2025-07-17 13:32:13 +0530

    intial repo structure

---
+++ src/main/java/com/email/replier/EmailReplierApplication.java
@@ -0,0 +1,13 @@
+package com.email.replier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailReplierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailReplierApplication.class, args);
+	}
+
+}
