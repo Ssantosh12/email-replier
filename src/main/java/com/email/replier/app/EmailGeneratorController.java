01dac1e41c84f8d408938d4b2cc9a43b6d4e14e2
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2025-08-30 15:18:23 +0530

    restructured folders

---
+++ src/main/java/com/email/replier/app/EmailGeneratorController.java
@@ -0,0 +1,15 @@
+package com.email.replier.app;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/email")
+public class EmailGeneratorController {
+    // generetae a method
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        return ResponseEntity.ok(body:"")
+    }
+}
